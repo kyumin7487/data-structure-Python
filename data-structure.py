09be504c4592fca4d4f12efce8346b89ea032528
박규민 <131234408+kyumin7487@users.noreply.github.com>
2023-11-17 10:05:56 +0900

    ⚡️: 퀵정렬 기능추가

--- data-structure.py
+++ data-structure.py
@@ -1,6 +1,9 @@
 import random
 
-def selection_sort(A): # 선택정렬
+S = 1 # 퀵정열 하는데 문제가 자꾸 생겨서 여기에다가 S 잡았어요
+
+def selection_sort(A): 
+    # 선택 정렬 알고리즘 구현
     n = len(A)
     for i in range(n - 1):
         least = i
@@ -8,39 +11,68 @@ def selection_sort(A): # 선택정렬
             if A[j] < A[least]:
                 least = j
         A[i], A[least] = A[least], A[i]
-        print("Step", i + 1, "=", A)
+        print("단계", i + 1, "=", A)
     # 정렬이 끝난 후 최종 결과 출력
     print("최종 정렬 결과:", A)
-    
-def insertion_sort(A): # 삽입정렬
+
+def insertion_sort(A): 
+    # 삽입 정렬 알고리즘 구현
     n = len(A)
-    for i in range(1,n) :
+    for i in range(1, n):
         key = A[i]
-        j = i-1
-        while j >= 0 and A[j] > key :
-            A[j+1] = A[j]
+        j = i - 1
+        while j >= 0 and A[j] > key:
+            A[j + 1] = A[j]
             j -= 1
-        A[j+1] = key
-        print("Step", i + 1, "=", A)
+        A[j + 1] = key
+        print("단계", i + 1, "=", A)
     # 정렬이 끝난 후 최종 결과 출력
     print("최종 정렬 결과:", A)
-    
-def bubble_sort(A): # 버블정렬
+
+def bubble_sort(A): 
+    # 버블 정렬 알고리즘 구현
     n = len(A)
-    for i in range(n-1, 0, -1) :
+    for i in range(n - 1, 0, -1):
         bChanged = False
-        for j in range(i) :
-            if A[j] > A[j+1] :
-                A[j], A[j+1] = A[j+1], A[j]
+        for j in range(i):
+            if A[j] > A[j + 1]:
+                A[j], A[j + 1] = A[j + 1], A[j]
                 bChanged = True
         if not bChanged:
             break
-        print("Step", n - i, "=", A)
-     # 정렬이 끝난 후 최종 결과 출력
+        print("단계", n - i, "=", A)
+    # 정렬이 끝난 후 최종 결과 출력
     print("최종 정렬 결과:", A)
 
+def quick_sort(A, left, right):
+    # 퀵 정렬 알고리즘 구현
+    global S
+    print("quick_sort(A,", left, right, ")")
+    if left < right:  
+        i = left + 1
+        j = right
+        pivot = A[left]
+        while i <= j:
+            while i <= right and A[i] <= pivot:
+                i += 1
+            while j >= left and A[j] > pivot:
+                j -= 1
+            if i < j:
+                A[i], A[j] = A[j], A[i]
+                print_qs(A)
+        A[left], A[j] = A[j], A[left]
+        print_qs(A)
+        quick_sort(A, left, j - 1)
+        quick_sort(A, j + 1, right)
 
-def main(): # 시작할때마다 이화면 띄우기 
+def print_qs(A):
+    global S
+    print("단계", S, "=", A)
+    S += 1
+    # 정렬이 끝난 후 최종 결과 출력
+    print("최종 정렬 결과:", A)
+
+def main():
     while True:
         print("""
         ***********************************
@@ -56,34 +88,39 @@ def main(): # 시작할때마다 이화면 띄우기
         ***********************************
         """)
 
-        # 사용자에게 정렬 방법을 선택하도록 함
         n = int(input("원하시는 정렬의 번호를 선택해 주세요 (종료하려면 7을 입력하세요): "))
 
-        # 7을 선택하면 프로그램 종료
         if n == 7:
             print("프로그램을 종료합니다.")
             break
-        
-        elif n == 1: # 선택정렬
+
+        elif n == 1:
             # 0 이상 100 이하 범위에서 중복되지 않는 25개의 랜덤 정수를 생성하여 리스트에 저장
             random_list = random.sample(range(101), 25)
             print("선택정렬을 실행합니다")
             print("원본 리스트:", random_list) # 원본 리스트 출력
-            selection_sort(random_list)  # 선택 정렬 함수 호출
-            
-        elif n == 2: # 삽입정렬
-             # 0 이상 100 이하 범위에서 중복되지 않는 25개의 랜덤 정수를 생성하여 리스트에 저장
+            selection_sort(random_list) # 선택정렬 함수 호출
+
+        elif n == 2:
+            # 0 이상 100 이하 범위에서 중복되지 않는 25개의 랜덤 정수를 생성하여 리스트에 저장
             random_list = random.sample(range(101), 25)
             print("삽입정렬을 실행합니다")
             print("원본 리스트:", random_list) # 원본 리스트 출력
-            insertion_sort(random_list) # 삽입 정렬 함수 호출
-        
-        elif n == 3: # 버블정렬
-             # 0 이상 100 이하 범위에서 중복되지 않는 25개의 랜덤 정수를 생성하여 리스트에 저장
+            insertion_sort(random_list) # 삽입정렬 함수 호출
+
+        elif n == 3:
+            # 0 이상 100 이하 범위에서 중복되지 않는 25개의 랜덤 정수를 생성하여 리스트에 저장
             random_list = random.sample(range(101), 25)
             print("버블정렬을 실행합니다")
-            print("원본 리스트:", random_list) # 원본 리스트 출력
-            bubble_sort(random_list) # 삽입 정렬 함수 호출
+            print("원본 리스트:", random_list)# 원본 리스트 출력
+            bubble_sort(random_list) # 버블정렬 함수 호출
+
+        elif n == 4:
+            # 0 이상 100 이하 범위에서 중복되지 않는 25개의 랜덤 정수를 생성하여 리스트에 저장
+            random_list = random.sample(range(101), 25)
+            print("퀵정렬을 실행합니다")
+            print("원본 리스트:", random_list)# 원본 리스트 출력
+            quick_sort(random_list, 0, len(random_list) - 1) # 퀵정렬 함수 호출
 
 if __name__ == "__main__":
     main()
